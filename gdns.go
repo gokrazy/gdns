2b97f2e4151b5e6cd370114be0a8b5337736fc2b
Michael Stapelberg <michael@stapelberg.de>
2021-01-30 16:31:24 +0100

    scan listening addresses every 5s

--- gdns.go
+++ gdns.go
@@ -23,11 +23,11 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/rtr7/dyndns"
 	"github.com/vishvananda/netlink"
 	"golang.org/x/net/idna"
-	"golang.org/x/sync/errgroup"
 	"golang.org/x/sys/unix"
 )
 
@@ -92,105 +92,107 @@ func logic() error {
 	}
 	eui = eui[strings.IndexByte(eui, ':'):]
 
-	addrs, err := listenaddrs(nil)
-	if err != nil {
-		return fmt.Errorf("listenaddrs: %v", err)
-	}
-	var eg errgroup.Group
 	used := make(map[string]bool)
-	for _, addr := range addrs {
-		// Assumption: a port is owned by one program.
-		// Assumption: each program listens on either:
-		// [::] (public) or (at least)
-		// [::1] (private)
-		if !addr.Addr.Equal(net.IPv6loopback) &&
-			!addr.Addr.Equal(net.IPv6unspecified) {
-			continue
-		}
-
-		port := fmt.Sprintf("%x", addr.Port)
-		proxyaddr := ulaPrefix + port + eui
-
-		cmdline := filepath.Base(string(addr.Cmdline))
-		if cmdline == "init" {
-			continue
-		}
-		name, err := idna.ToASCII(cmdline)
+	for ; ; time.Sleep(5 * time.Second) {
+		addrs, err := listenaddrs(nil)
 		if err != nil {
-			return err
+			return fmt.Errorf("listenaddrs: %v", err)
 		}
-		{
-			uniq := name
-			for i := 2; used[uniq]; i++ {
-				uniq = name + "." + strconv.Itoa(i)
+		for _, addr := range addrs {
+			// Assumption: a port is owned by one program.
+			// Assumption: each program listens on either:
+			// [::] (public) or (at least)
+			// [::1] (private)
+			if !addr.Addr.Equal(net.IPv6loopback) &&
+				!addr.Addr.Equal(net.IPv6unspecified) {
+				continue
 			}
-			name = uniq
-		}
-		used[name] = true
-		log.Printf("  %v %v → %v",
-			net.JoinHostPort(addr.Addr.String(), port),
-			name,
-			proxyaddr,
-		)
-		if listeners[proxyaddr] {
-			continue // already set up
-		}
-		listeners[proxyaddr] = true
 
-		{
-			// add IP address to eth0 if not already present
-			addr, err := netlink.ParseAddr(proxyaddr + "/48")
-			if err != nil {
-				return fmt.Errorf("netlink.ParseAddr(%v): %v", proxyaddr, err)
+			port := fmt.Sprintf("%x", addr.Port)
+			proxyaddr := ulaPrefix + port + eui
+
+			cmdline := filepath.Base(string(addr.Cmdline))
+			if cmdline == "init" {
+				continue
 			}
-			// Skip duplicate address detection so that we can immediately
-			// listen on this address after configuring:
-			addr.Flags |= unix.IFA_F_NODAD
-			link, err := netlink.LinkByName("eth0") // TODO
+			name, err := idna.ToASCII(cmdline)
 			if err != nil {
-				link, err = netlink.LinkByName("lan0") // TODO
+				return err
+			}
+			if listeners[proxyaddr] {
+				continue // already set up
+			}
+			listeners[proxyaddr] = true
+			{
+				uniq := name
+				for i := 2; used[uniq]; i++ {
+					uniq = name + "." + strconv.Itoa(i)
+				}
+				name = uniq
+			}
+			used[name] = true
+			log.Printf("  %v %v → %v",
+				net.JoinHostPort(addr.Addr.String(), port),
+				name,
+				proxyaddr,
+			)
+
+			{
+				// add IP address to eth0 if not already present
+				addr, err := netlink.ParseAddr(proxyaddr + "/48")
 				if err != nil {
-					return fmt.Errorf("netlink.LinkByName(eth0): %v", err)
+					return fmt.Errorf("netlink.ParseAddr(%v): %v", proxyaddr, err)
+				}
+				// Skip duplicate address detection so that we can immediately
+				// listen on this address after configuring:
+				addr.Flags |= unix.IFA_F_NODAD
+				link, err := netlink.LinkByName("eth0") // TODO
+				if err != nil {
+					link, err = netlink.LinkByName("lan0") // TODO
+					if err != nil {
+						return fmt.Errorf("netlink.LinkByName(eth0): %v", err)
+					}
+				}
+				if err := netlink.AddrReplace(link, addr); err != nil {
+					return fmt.Errorf("netlink.AddrAdd(%v, %v): %v", link, addr, err)
 				}
 			}
-			if err := netlink.AddrReplace(link, addr); err != nil {
-				return fmt.Errorf("netlink.AddrAdd(%v, %v): %v", link, addr, err)
+
+			// Set up listener and serve HTTP:
+			ln, err := net.Listen("tcp", net.JoinHostPort(proxyaddr, "80"))
+			if err != nil {
+				log.Print(err)
+				continue
+			}
+
+			target, err := url.Parse("http://[::1]:" + fmt.Sprintf("%d", addr.Port))
+			if err != nil {
+				log.Print(err)
+				continue
+			}
+
+			localAddr, err := net.ResolveTCPAddr("tcp6", net.JoinHostPort(proxyaddr, "0"))
+			if err != nil {
+				log.Print(err)
+				continue
+			}
+			proxy := httputil.NewSingleHostReverseProxy(target)
+			proxy.Transport = &http.Transport{
+				Dial: (&net.Dialer{
+					LocalAddr: localAddr,
+				}).Dial,
+			}
+			go func() {
+				if err := http.Serve(ln, proxy); err != nil {
+					log.Fatal(err)
+				}
+			}()
+
+			if err := dyndns.SetSubname(name, net.ParseIP(proxyaddr)); err != nil {
+				return fmt.Errorf("dyndns.SetSubname(%q, %v): %v", name, proxyaddr, err)
 			}
 		}
-
-		// Set up listener and serve HTTP:
-		ln, err := net.Listen("tcp", net.JoinHostPort(proxyaddr, "80"))
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-
-		target, err := url.Parse("http://[::1]:" + fmt.Sprintf("%d", addr.Port))
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-
-		localAddr, err := net.ResolveTCPAddr("tcp6", net.JoinHostPort(proxyaddr, "0"))
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-		proxy := httputil.NewSingleHostReverseProxy(target)
-		proxy.Transport = &http.Transport{
-			Dial: (&net.Dialer{
-				LocalAddr: localAddr,
-			}).Dial,
-		}
-		eg.Go(func() error {
-			return http.Serve(ln, proxy)
-		})
-
-		if err := dyndns.SetSubname(name, net.ParseIP(proxyaddr)); err != nil {
-			return fmt.Errorf("dyndns.SetSubname(%q, %v): %v", name, proxyaddr, err)
-		}
 	}
-	return eg.Wait()
 }
 
 func main() {
